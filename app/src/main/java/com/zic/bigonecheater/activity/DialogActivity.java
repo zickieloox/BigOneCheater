cae652a91d74bd4a9ec97dd07d227538469e3860
zickieloox <zickieloox@gmail.com>
2017-03-26 23:00:48 +0700

    replace tasks with start activities.

--- app/src/main/java/com/zic/bigonecheater/activity/DialogActivity.java
+++ app/src/main/java/com/zic/bigonecheater/activity/DialogActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,9 +20,7 @@ import com.zic.bigonecheater.utils.AppUtils;
 import com.zic.bigonecheater.utils.FileUtils;
 import com.zic.bigonecheater.utils.PrefsUtils;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DialogActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -95,27 +94,13 @@ public class DialogActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     public void installApps() {
-        File bigOneAppsDir = new File(boAppsPath);
-        List<String> apks = new ArrayList<>();
-        for (File apkFile : bigOneAppsDir.listFiles()) {
-            String apkFilePath = apkFile.getAbsolutePath();
-            //String fileName = apkFile.getName();
-
-            apks.add(apkFilePath);
-        }
-        new InstallTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, apks);
+        Intent installIntent = new Intent(DialogActivity.this, InstallActivity.class);
+        startActivity(installIntent);
     }
 
     public void uninstallApps() {
-        File bigOneAppsDir = new File(boAppsPath);
-        List<String> pkgNames = new ArrayList<>();
-        for (File apkFile : bigOneAppsDir.listFiles()) {
-            String apkFilePath = apkFile.getAbsolutePath();
-            String pkgName = AppUtils.getPkgNameFromApk(this, apkFilePath);
-
-            pkgNames.add(pkgName);
-        }
-        new UninstallTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, pkgNames);
+        Intent uninstallIntent = new Intent(DialogActivity.this, UninstallActivity.class);
+        startActivity(uninstallIntent);
     }
 
     public void prevFid() {
@@ -174,43 +159,6 @@ public class DialogActivity extends AppCompatActivity implements View.OnClickLis
 
         tvFid.setText(getString(R.string.tv_fb_id) + fid);
         tvPassword.setText(getString(R.string.tv_fb_pass) + password);
-    }
-
-    private class InstallTask extends AsyncTask<List<String>, Void, String> {
-
-        @SafeVarargs
-        @Override
-        protected final String doInBackground(List<String>... params) {
-            List<String> apks = params[0];
-            for (int i = 0; i < apks.size(); i++) {
-                String apkFilePath = apks.get(i);
-                AppUtils.installAsRoot(apkFilePath);
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            Toast.makeText(DialogActivity.this, getString(R.string.toast_installed), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private class UninstallTask extends AsyncTask<List<String>, Void, String> {
-
-        @SafeVarargs
-        @Override
-        protected final String doInBackground(List<String>... params) {
-            List<String> pkgNames = params[0];
-            for (int i = 0; i < pkgNames.size(); i++) {
-                AppUtils.uninstallAsRoot(pkgNames.get(i));
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            Toast.makeText(DialogActivity.this, getString(R.string.toast_uninstalled), Toast.LENGTH_SHORT).show();
-        }
     }
 
     private class NewBigOneTask extends AsyncTask {
